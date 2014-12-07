5fc9da26cdc98ea19499e595abb718bc9753eb14
Valentin KIM <kim.dara@gmail.com>
2014-12-07 21:48:23 +0100

    ajout de consultation et de contact

---
+++ pages/contact.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>UDS BAT</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="css/app.css">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+	</head>
+	<body class = "jumbotron">
+		<ul class="nav nav-pills m-title">
+			<li role="presentation"><a href="index.php">Ajout</a></li>
+			<li role="presentation"><a href="liste.php">Liste</a></li>
+			<li role="presentation" class="active"><a href="#">Contact</a></li>
+		</ul>
+	</body>
+	<script src = "js/jquery-2.1.1.min.js"></script>
+	<script src = "js/bootstrap.min.js"></script>
+	<script src = "js/app.js"></script>
+	<script src = "js/load.anim.js"></script>
+</html>
